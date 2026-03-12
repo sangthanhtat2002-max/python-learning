eb4459279f7132c52b62119e6857e686172c09a2
sangthanhtat2002-max <sangthanhtat2002@gmail.com>
2026-03-12 16:17:46 +0700

    Delete bai1.py

--- bai1.py
+++
@@ -1,8 +0,0 @@
-# Có try/except → chương trình không crash
-try:
-    diem = int("abc")   # thử chạy dòng này
-    print(diem)
-except:
-    print("Lỗi! Dữ liệu không hợp lệ")  # nếu lỗi → chạy dòng này
-
-print("Chương trình vẫn tiếp tục chạy!")
